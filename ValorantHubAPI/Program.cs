84f373898e1b7e79b65c527f55cc6013950fbbbb
José Ferrari <licoverpa321@gmail.com>
2024-11-05 23:39:01 -0300

    might be the problem

--- ValorantHubAPI/Program.cs
+++ ValorantHubAPI/Program.cs
@@ -18,12 +18,13 @@ builder.Services.AddScoped<IWeaponService, WeaponService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+/*
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+*/
 app.UseHttpsRedirection();
 
 app.MapControllers();
