9605bd3b0869667ecdc328e84f92cbfb92a5ce5f
José Ferrari <licoverpa321@gmail.com>
2024-11-28 23:18:53 -0300

    Login Initial Create

---
+++ ValorantHubAPI/API/Services/LoginService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ValorantHubAPI.API.Services
+{
+    public class LoginService : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
