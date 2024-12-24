f6d3079303f6c90aa5ff28d07299231542fe5e4d
José Ferrari <licoverpa321@gmail.com>
2024-12-24 00:22:21 -0300

    Adding first unit tests for the back end

---
+++ ValorantHubAPI.tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+namespace ValorantHubAPI.tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            var expected = 5;
+
+            // Act
+            var result = 2 + 3;
+
+            // Assert
+            Assert.Equal(expected, result);
+
+        }
+    }
+}
